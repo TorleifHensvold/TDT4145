e37e94e67610f326ecc8a2e8927130059f750033
TorleifHensvold <torleinh@stud.ntnu.no>
2018-03-19 10:42:23 +0100

    Added settings for connecting to NTNU database

---
+++ Oving3/src/main/resources/database/DataBaseSettings.java
@@ -0,0 +1,10 @@
+package database;
+
+public class DataBaseSettings 
+{
+	public static final String databaseServer = "mysql.stud.ntnu.no";
+    public static final String databaseName = "torleinh_db_project";
+    public static final String databaseUsername = "torleinh_user";
+    public static final String databaseSPassword = "Da5pmFgH";
+
+}
