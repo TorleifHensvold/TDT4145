b3aac1be9c2b59f91fb7e05b62121947d30dc5f4
elisahet <elisahet@stud.ntnu.no>
2018-03-20 15:51:02 +0100

    More testing, test adding a new apparat

--- Oving3/src/main/java/tdt4145/gr125/Oving3/App.java
+++ Oving3/src/main/java/tdt4145/gr125/Oving3/App.java
@@ -2,6 +2,7 @@ package tdt4145.gr125.Oving3;
 
 import java.sql.*;
 
+import database.service.ApparatService;
 import database.service.DatabaseService;
 import table.Apparat;
 
@@ -17,32 +18,7 @@ public class App
 		System.out.println("java version: " + System.getProperty("java.version"));
 		System.out.println("javafx.version: " + System.getProperty("javafx.version"));
 
-		try 
-		{
-			Connection conn = DatabaseService.getDatasource().getConnection();
-			PreparedStatement prepState = conn.prepareStatement("SELECT * FROM apparat");
-			ResultSet rs = prepState.executeQuery();
-			ResultSetMetaData meta = rs.getMetaData();
-			int columnCount = meta.getColumnCount();
-			for (int i = 1; i <= columnCount; i++) 
-			{
-				System.out.printf(meta.getColumnName(i) + ", ");
-			}
-			while (rs.next()) 
-			{
-				System.out.println();
-				String row = "";
-				for (int i = 1; i <= columnCount; i++) 
-				{
-				row += rs.getString(i) + ", ";	
-				}
-				System.out.println(row);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
+				try {
 		String navn = "Pull-up Bar";
 		Connection con= DatabaseService.getDatasource().getConnection();
         PreparedStatement prepstatement = con.prepareStatement("SELECT * from apparat WHERE Navn=?;");
@@ -71,6 +47,37 @@ public class App
 		catch (Exception e)
 		{System.out.println(e);
 		}
-		
+		try {
+			Apparat ap = new Apparat("Stol", "Til å sitte på.");
+			ApparatService.NewApparat(ap);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		try 
+		{
+			Connection conn = DatabaseService.getDatasource().getConnection();
+			PreparedStatement prepState = conn.prepareStatement("SELECT * FROM apparat");
+			ResultSet rs = prepState.executeQuery();
+			ResultSetMetaData meta = rs.getMetaData();
+			int columnCount = meta.getColumnCount();
+			for (int i = 1; i <= columnCount; i++) 
+			{
+				System.out.printf(meta.getColumnName(i) + ", ");
+			}
+			while (rs.next()) 
+			{
+				System.out.println();
+				String row = "";
+				for (int i = 1; i <= columnCount; i++) 
+				{
+				row += rs.getString(i) + ", ";	
+				}
+				System.out.println(row);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
