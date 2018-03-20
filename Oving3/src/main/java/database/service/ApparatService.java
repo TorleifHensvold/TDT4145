469362b1ac7909eb8dfaf15ab0b7f3fcfdd3c977
elisahet <elisahet@stud.ntnu.no>
2018-03-20 15:01:25 +0100

    create a function to insert new apparat, NewApparat

--- Oving3/src/main/java/database/service/ApparatService.java
+++ Oving3/src/main/java/database/service/ApparatService.java
@@ -1,5 +1,32 @@
 package database.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import table.Apparat;
+
+
 public class ApparatService {
+	//hent ut alle apparater
+	//legge inn nye apparater
+	//fjerne apparater
+	//hente ut beskrivelsen til ett apparat
 	
+	public boolean NewApparat(Apparat apparat) {
+		try {
+			Connection conn = DatabaseService.getDatasource().getConnection();
+			PreparedStatement prepStatement = conn.prepareStatement("INSERT INTO Apparat (Navn, Beskrivelse) VALUES (?,?);");
+			prepStatement.setString(1, apparat.getNavn());
+			prepStatement.setString(2, apparat.getBeskrivelse());
+			
+			boolean success = prepStatement.execute();
+			
+			prepStatement.close();
+			conn.close();
+			return success;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
