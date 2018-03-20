c729b78ca20099ed7ef5e2809807795227a46d54
annabakkebo <annabakk@stud.ntnu.no>
2018-03-20 10:21:35 +0100

    Made class for treningsokt in table

---
+++ Oving3/src/main/java/table/treningsokt.java
@@ -0,0 +1,11 @@
+package table;
+
+public class treningsokt {
+	public int oktID;
+	public String dato;
+	public String tidspunkt;
+	public int varighet;
+	public int form;
+	public int prestasjon;
+	public String notat;
+}
