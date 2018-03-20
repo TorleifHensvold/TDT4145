2c5ffddde65759503711b33a9c188250edf2d08a
TorleifHensvold <torleif.hensvold@outlook.com>
2018-03-20 12:08:39 +0100

    Updated fxml-files to use fxml version 8.0.1
    Added debugging to App.java

--- Oving3/src/main/java/tdt4145/gr125/Oving3/App.java
+++ Oving3/src/main/java/tdt4145/gr125/Oving3/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("java version: "+System.getProperty("java.version"));
+        System.out.println("javafx.version: " + System.getProperty("javafx.version"));
     }
 }
