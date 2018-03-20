7b4eae0fc2d7126d722f2d6cb2b21a42bb42f6a8
TorleifHensvold <torleif.hensvold@outlook.com>
2018-03-20 13:06:10 +0100

    Created constructor, getters and setters for inneholder.java
    Added TODO statements for the SQL parts.

--- Oving3/src/main/java/table/inneholder.java
+++ Oving3/src/main/java/table/inneholder.java
@@ -1,6 +1,53 @@
 package table;
 
-public class inneholder {
+public class inneholder 
+{
 	public int OktID; 	//primary key and foreign key to treningsokt
 	public int OvelsesID;	//primary key and foreign key to ovelse
+	
+	
+	public inneholder(int OktID, int OvelsesID) throws Exception
+	{
+		setOktID(OktID);
+		setOvelsesID(OvelsesID);
+	}
+	
+	public int getOktID() 
+	{
+		return OktID;
+	}
+	
+	public int getOvelsesID() 
+	{
+		return OvelsesID;
+	}
+	
+	public void setOktID(int oktID) throws Exception
+	{
+		if (oktID > 1000000000)
+		{
+			throw new IllegalArgumentException("The number is too high to be an integer in java.");
+		}
+		// TODO: Create else if (oktID does not exist) then Exeption
+		else
+		{
+		this.OktID = oktID;
+		}
+	}
+	
+	public void setOvelsesID(int ovelsesID) throws Exception
+	{
+		if (ovelsesID > 1000000000)
+		{
+			throw new IllegalArgumentException("The number is too high to be an integer in java.");
+		}
+		// TODO: Create else if (oktID does not exist) then Exeption
+		else
+		{
+		this.OvelsesID = ovelsesID;
+		}
+	}
+	
+	// TODO Create empty constructor and a way to initialise a inneholder class from SQL server.
+	
 }
