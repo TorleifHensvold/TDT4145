275056119f7867d96c1666ab7bb2bf307d5a6e73
TorleifHensvold <torleif.hensvold@outlook.com>
2018-03-20 13:21:54 +0100

    Added getters and setters for treningsokt.java

--- Oving3/src/main/java/table/treningsokt.java
+++ Oving3/src/main/java/table/treningsokt.java
@@ -1,11 +1,82 @@
 package table;
 
-public class treningsokt {
-	public int OktID;
-	public String Dato;
-	public String Tidspunkt;
-	public int Varighet;
-	public int Form;
-	public int Prestasjon;
-	public String Notat;
+public class treningsokt 
+{
+	private int OktID;
+	private String Dato;
+	private String Tidspunkt;
+	private int Varighet;
+	private int Form;
+	private int Prestasjon;
+	private String Notat;
+	
+	// ---------------------- Getters ------------------------------- //
+	
+	public int getOktID() 
+	{
+		return this.OktID;
+	}
+	
+	public String getDato() 
+	{
+		return this.Dato;
+	}
+	
+	public String getTidspunkt() 
+	{
+		return this.Tidspunkt;
+	}
+	
+	public int getVarighet() 
+	{
+		return this.Varighet;
+	}
+	
+	public int getForm() 
+	{
+		return this.Form;
+	}
+	
+	public int getPrestasjon() 
+	{
+		return this.Prestasjon;
+	}
+	
+	public String getNotat() 
+	{
+		return this.Notat;
+	}
+	
+	// --------------------------- Setters ------------------ //
+	
+	public void setOktID(int oktID) 
+	{
+		OktID = oktID;
+	}
+	public void setDato(String dato) 
+	{
+		Dato = dato;
+	}
+	public void setTidspunkt(String tidspunkt) 
+	{
+		Tidspunkt = tidspunkt;
+	}
+	public void setVarighet(int varighet) 
+	{
+		Varighet = varighet;
+	}
+	public void setForm(int form) 
+	{
+		Form = form;
+	}
+	public void setPrestasjon(int prestasjon) 
+	{
+		Prestasjon = prestasjon;
+	}
+	public void setNotat(String notat) 
+	{
+		Notat = notat;
+	}
+	
+	
 }
