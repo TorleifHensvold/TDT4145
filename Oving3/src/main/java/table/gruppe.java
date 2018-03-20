facf4fbdfd09f506b9b6ff645364ebf22d0db7bb
TorleifHensvold <torleif.hensvold@outlook.com>
2018-03-20 12:35:46 +0100

    Added constructor and setters to gruppe.java

--- Oving3/src/main/java/table/gruppe.java
+++ Oving3/src/main/java/table/gruppe.java
@@ -1,6 +1,34 @@
 package table;
 
-public class gruppe {
-	public int GruppeID;
-	public String TypeOvelse; 
+public class gruppe 
+{
+	private int GruppeID;
+	private String TypeOvelse;
+	
+	public void gruppe(int GruppeID, String TypeOvelse) throws Exception
+	{
+		setGruppeID(GruppeID);
+		setTypeOvelse(TypeOvelse);
+	}
+
+	private void setTypeOvelse(String typeOvelse) throws Exception
+	{
+		if (typeOvelse.length() > 50)
+		{
+			throw new IllegalArgumentException("The sentence must be 50 characters or less.");
+		}
+		else
+		{
+			this.TypeOvelse = typeOvelse;
+		}
+	}
+
+	private void setGruppeID(int gruppeID) 
+	{
+		// TODO: add to check whether GruppeID already exists in SQL database.
+		
+		this.GruppeID = gruppeID;
+	}
+	
+	
 }
