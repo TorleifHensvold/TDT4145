c361b8e9b916ff1d5af0efe0ef64f94df084755b
elisahet <elisahet@stud.ntnu.no>
2018-03-20 13:50:01 +0100

    set and get for undergruppe

--- Oving3/src/main/java/table/undergruppe.java
+++ Oving3/src/main/java/table/undergruppe.java
@@ -1,6 +1,29 @@
 package table;
 
 public class undergruppe {
-	public int GruppeID;
-	public int UnderGruppeID;
+	private int GruppeID;
+	private int UnderGruppeID;
+	
+	public int getGruppeID() {
+		return GruppeID;
+	}
+	public void setGruppeID(int gruppeID) {
+		if (gruppeID <= 1000000000) { //TODO: Create else if (gruppeID does not exist) then Exception
+			GruppeID = gruppeID;
+		}else {
+			throw new IllegalArgumentException("The number is too high to be an integer in java.");
+		}
+	}
+	public int getUnderGruppeID() {
+		return UnderGruppeID;
+	}
+	public void setUnderGruppeID(int underGruppeID) {
+		if (underGruppeID <= 1000000000) {
+			UnderGruppeID = underGruppeID;
+		} else { //TODO: Create else if (underGruppeID does not exist) then Exception, also should not be the same as gruppeID
+			throw new IllegalArgumentException("The number is too high to be an integer in java.");
+		}
+	}
+	
+	
 }
