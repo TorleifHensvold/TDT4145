51c9a334f43dea7153dfede693dc5fe146473ef6
TorleifHensvold <torleif.hensvold@outlook.com>
2018-03-22 21:46:14 +0100

    Formatting

--- Oving3/src/main/java/table/Ovelse.java
+++ Oving3/src/main/java/table/Ovelse.java
@@ -1,65 +1,67 @@
 package table;
 
-public class Ovelse {
+public class Ovelse
+{
 	private int ovelsesID;
 	private String navn;
-	
-	
-	
-	public Ovelse(int ovelsesID, String navn) throws Exception{
+
+	public Ovelse(int ovelsesID, String navn) throws Exception
+	{
 		setOvelsesID(ovelsesID);
 		setNavn(navn);
 	}
 
 	public Ovelse()
 	{
-		
+
 	}
-	
+
 	public void databaseSetOvelsesID(int ovelsesID)
 	{
 		this.ovelsesID = ovelsesID;
 	}
-	
+
 	public void databaseSetNavn(String navn)
 	{
 		this.navn = navn;
 	}
 
-
-	public int getOvelsesID() {
+	public int getOvelsesID()
+	{
 		return this.ovelsesID;
 	}
 
-
-
-	public void setOvelsesID(int ovelsesID) throws Exception{
-		if (ovelsesID <= 1000000000) {
-			this.ovelsesID=ovelsesID;
+	public void setOvelsesID(int ovelsesID) throws Exception
+	{
+		if (ovelsesID <= 1000000000)
+		{
+			this.ovelsesID = ovelsesID;
 		}
-		else {
+		else
+		{
 			throw new IllegalArgumentException("the int is too long");
 		}
 	}
 
-
-
-	public String getNavn() {
+	public String getNavn()
+	{
 		return this.navn;
 	}
 
-
-
-	public void setNavn(String navn) throws Exception{
-		if (navn ==null) {
+	public void setNavn(String navn) throws Exception
+	{
+		if (navn == null)
+		{
 			throw new IllegalArgumentException("Can't be null");
 		}
-		else if(navn.length()<=50) {
+		else if (navn.length() <= 50)
+		{
 			this.navn = navn;
 		}
-		else {
+		else
+		{
 			throw new IllegalArgumentException("The name is too long");
 		}
 	}
-	
+
 }
