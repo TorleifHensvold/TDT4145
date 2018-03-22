80d920cd8178db241de1595f37326b9713a59ea5
annabakkebo <annabakk@stud.ntnu.no>
2018-03-22 16:14:10 +0100

    Created nyTreningsOkt method
    The method creates a treningsokt into database, returns true if it
    works, and false if not

--- Oving3/src/main/java/database/service/TreningsOktService.java
+++ Oving3/src/main/java/database/service/TreningsOktService.java
@@ -1,6 +1,33 @@
 package database.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import table.Treningsokt;
+
 public class TreningsOktService {
 	
+	public static boolean nyTreningsOkt(Treningsokt treningsokt) {
+		try {
+			Connection conn = DatabaseService.getDatasource().getConnection();
+			PreparedStatement prepStatement = conn.prepareStatement("INSERT INTO treningsokt (OktID, Dato, Tidspunkt, Varighet, Form, Prestasjon, Notat) VALUES (?,?,?,?,?,?,?)");
+			prepStatement.setInt(1, treningsokt.getOktID());
+			prepStatement.setString(2, treningsokt.getDato());
+			prepStatement.setString(3, treningsokt.getTidspunkt());
+			prepStatement.setInt(4, treningsokt.getVarighet());
+			prepStatement.setInt(5, treningsokt.getForm());
+			prepStatement.setInt(6, treningsokt.getPrestasjon());
+			prepStatement.setString(7, treningsokt.getNotat());
+			boolean success = prepStatement.execute();
+			
+			prepStatement.close();
+			conn.close();
+			return success;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	
+	}
 }
