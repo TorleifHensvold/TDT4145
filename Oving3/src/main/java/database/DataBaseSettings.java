1e28a62542d7ce56c0b42b0331ca314a2db45dcc
TorleifHensvold <torleif.hensvold@outlook.com>
2018-05-31 19:00:39 +0200

    Update DataBaseSettings.java

--- Oving3/src/main/java/database/DataBaseSettings.java
+++ Oving3/src/main/java/database/DataBaseSettings.java
@@ -2,9 +2,9 @@ package database;
 
 public class DataBaseSettings 
 {
-	public static final String databaseServer = "mysql.stud.ntnu.no";
-    public static final String databaseName = "torleinh_db_project";
-    public static final String databaseUsername = "torleinh_user";
-    public static final String databaseSPassword = "Da5pmFgH";
+	public static final String databaseServer = "";
+    public static final String databaseName = "";
+    public static final String databaseUsername = "";
+    public static final String databasePassword = "";
 
 }
